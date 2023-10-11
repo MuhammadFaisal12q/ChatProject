939fa72bc1c7a6664a44a93134e1ce34b842f431
CHAsif99 <chasifidrees@gmail.com>
2023-10-11 15:29:27 +0500

    add some text in this screen

--- video.js
+++ video.js
@@ -5,6 +5,17 @@ const video = () => {
   return (
     <View>
       <Text>this is video screen  the video shows on this </Text>
+
+
+<Text>1</Text>
+<Text>2</Text>
+
+<Text>3</Text>
+<Text>4</Text>
+<Text>5</Text>
+<Text>6</Text>
+
+
     </View>
   )
 }
